d81b206df3b1484c98b29b7c74e393327a0f67f7
ChrisKlecker <32399032+ChrisKlecker@users.noreply.github.com>
2018-03-09 13:33:57 -0500

    Continued work on UI. The Chapter selection will now alter the exercise selection.

--- src/java/TeamExerciseCSCI5520/JavaExercises.java
+++ src/java/TeamExerciseCSCI5520/JavaExercises.java
@@ -5,9 +5,19 @@
  */
 package TeamExerciseCSCI5520;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import javax.inject.Named;
 import javax.enterprise.context.SessionScoped;
 import java.io.Serializable;
+import java.util.ArrayList;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ValueChangeEvent;
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
 
 /**
  *
@@ -20,7 +30,136 @@ public class JavaExercises implements Serializable {
     /**
      * Creates a new instance of JavaExercises
      */
-    public JavaExercises() {
+    private ArrayList<String> Chapters;
+    private ArrayList<String> Exercises;
+    private ArrayList<String> AllExercises;
+    private String ExerciseSelected;
+    private String ChapterSelected;
+    private String DataFile;
+    private String Comment;
+
+    public ArrayList<String> getAllExercises() {
+        return AllExercises;
+    }
+
+    public void setAllExercises(ArrayList<String> AllExercises) {
+        this.AllExercises = AllExercises;
+    }
+
+    public String getComment() {
+        return Comment;
+    }
+
+    public void setComment(String Comment) {
+        this.Comment = Comment;
+    }
+
+    public String getDataFile() {
+        return DataFile;
+    }
+
+    public void setDataFile(String DataFile) {
+        this.DataFile = DataFile;
     }
     
+    public String getChapterSelected() {
+        
+        return ChapterSelected;
+    }
+
+    public void setChapterSelected(String ChapterSelected) {
+        
+        this.ChapterSelected = ChapterSelected;
+        
+        ResetExercises();
+    }
+
+    public ArrayList<String> getChapters(){
+
+        return Chapters;
+    }
+    public void setChapters(ArrayList<String> chapters){
+
+        this.Chapters = chapters;
+    }
+    
+    public ArrayList<String> getExercises(){
+        
+        return Exercises;
+    }
+    
+    public void setExercises(ArrayList<String> exercises){
+        
+        this.Exercises = exercises;
+    }
+
+    public String getExerciseSelected() {
+        
+        return ExerciseSelected;
+    }
+
+    public void setExerciseSelected(String ExerciseSelected) {
+        
+        this.ExerciseSelected = ExerciseSelected;
+    }
+   
+   
+    public JavaExercises() throws IOException {
+        ServletContext ctx  = (ServletContext) FacesContext.getCurrentInstance().getExternalContext().getContext();
+        String realPath     = ctx.getRealPath("/");
+        setDataFile(realPath + "ags10e/exercisedescription");
+        
+        Chapters            = new ArrayList<>();
+        Exercises           = new ArrayList<>();
+        AllExercises        = new ArrayList<>();
+        ExerciseSelected    = "Exercise01_01";
+        ChapterSelected     = "Chapter 1";
+
+        Initialize();
+    }
+    
+    public void Initialize() throws FileNotFoundException, IOException{
+        
+        File f                  = new File(getDataFile());
+        File[] fList            = f.listFiles(); 
+        
+        for (File file : fList){
+            String FileName = file.getName();
+            String[] str    = FileName.split("_")[0].split("Exercise");
+            if(str.length>0){
+                int chapterNo = Integer.parseInt(str[1]);
+                if(Chapters.contains("Chapter "+chapterNo) == false)
+                    Chapters.add("Chapter "+chapterNo);
+                
+                AllExercises.add(FileName);
+            }
+        }
+        
+        ResetExercises();
+    }
+    
+    public void ResetExercises(){
+        
+        Exercises = new ArrayList<>();
+        
+        for (int i=0; i<AllExercises.size(); i++){
+            
+            String FileName = AllExercises.get(i);
+            String[] str    = FileName.split("_")[0].split("Exercise");
+            if(str.length>0){
+                int chapterNo = Integer.parseInt(str[1]);
+
+                if(Integer.parseInt(ChapterSelected.split("Chapter")[1].trim()) == chapterNo)
+                    Exercises.add(FileName);
+            }
+        }
+    }
+    
+    public void CompileRun(){
+        
+    }
+    
+    public void AutomaticCheck(){
+        
+    }
 }
